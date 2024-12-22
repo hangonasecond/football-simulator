2cb6fa211bef991daa034d8172ca959a97125880
Nik Armstrong <nikalas.armstrong@outlook.com>
2024-12-22 22:43:43 +1100

    Further split out core functionality

--- core.py
+++ core.py
@@ -1,3 +1,5 @@
+from sys import exit
+
 def check_input(prompt):
     while True:
         temp = input(prompt)
@@ -9,3 +11,6 @@ def check_input(prompt):
         else:
             print('Invalid input. Please try again.')
 
+def exit_message():
+    print('Alright. Thanks for trying football manager.')
+    exit()
--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import sys
 import players as pl
-from check_input import check_input
+import core
 
 print('Welcome to football manager!')
 
-if check_input('Would you like to create a new team (y/n)? '):
-    team_first = pl.Team()
+if core.check_input('Would you like to create a new team (y/n)? '):
+    player_team = pl.Team()
     print('The first team is: \n')
-    print(team_first)
+    print(player_team)
 else:
-    print('Alright. Thanks for trying football manager.')
-    sys.exit()
+    core.exit_message()
 
+if core.check_input('Would you like to play a game with this team (y/n)? '):
+    print('Great, let\'s play!')
+    print('Creating opposition...')
 
+    opp_team = pl.Team()
+
+    print('Your opposition is: \n')
+    print(opp_team)
+elif core.check_input('Would you like to generate a new team (y/n)? '):
+    print('Signing new players...')
+    player_team = pl.Team()
+else:
+    core.exit_message() 
