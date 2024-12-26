f13352d91e5c0e63b9458eedbb4ba5b02cb328d8
Nik Armstrong <nikalas.armstrong@outlook.com>
2024-12-26 22:53:05 +1100

    Remove hard-coding of formation info

--- modules/teams.py
+++ modules/teams.py
@@ -5,6 +5,7 @@ from statistics import mean
 
 class Team():
     def __init__(self, name):
+        self.set_formation()
         self.players = self.generate_players()
         self.name = name;
 
@@ -38,11 +39,11 @@ class Team():
             names = player_name()
             if i == 0:
                 players.append(pl.Goalkeeper(names[0], names[1], 200, 90, 32))
-            elif i in range(1, 5):
+            elif i in range(1, 1 + self.formation[0]):
                 players.append(pl.Defender(names[0], names[1], 190, 100, 29))
-            elif i in range(5, 9):
+            elif i in range(1 + self.formation[0], 1 + self.formation[0] + self.formation[1]):
                 players.append(pl.Midfielder(names[0], names[1], 180, 80, 26))
-            elif i in range(9, 11):
+            elif i in range(11 - self.formation[2], 11):
                 players.append(pl.Attacker(names[0], names[1], 150, 100, 22))
             else:
                 print('Player out of bounds')
@@ -57,3 +58,5 @@ class Team():
 
         self.rating = int(mean(player_ratings))
 
+    def set_formation(self):
+        self.formation = [4, 4, 2]
